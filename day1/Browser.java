cc831d20bb6b0f1d71fbd4da287c165eb2be8f4f
Archana-3191 <147937263+Archana-3191@users.noreply.github.com>
2023-10-14 18:00:41 +0100

    Add files via upload
    Day1

---
+++ day1/Browser.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Browser {
+	
+	public static void main(String[] args) {
+		
+		 float browserVersion =80.55f;
+		 String browserName = "FireFox";
+		 boolean pageLoaded= true;
+		 int noOfBrowser= 10;
+		 char browserLogo = 'F';
+		 System.out.println(browserVersion);
+		 System.out.println(browserName);
+		 System.out.println(pageLoaded);
+		 System.out.println(noOfBrowser);
+		 System.out.println(browserLogo);
+	}
+
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int personAge = 45;
+		if( personAge>0)
+		{
+			System.out.println("This number is positive");
+			
+		}
+		else
+		{
+			System.out.println("The number is negative");
+		}
+	}
+	
+
+}
